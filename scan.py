b44987379d8b8b256401c97d8bfb201a84162247
Evan Hermenau <evan@fetchlabs.io>
2026-08-02 14:38:51 -0400

    returning json data from API, accessing fields and parsing

--- scan.py
+++ scan.py
@@ -19,14 +19,14 @@ def checkHealth(base_url: str) -> None:
 
 def loadFile(file: str):
     with open(file, mode="r") as filehandler:
-        return json.dumps(filehandler.read())
+        return filehandler.read()
 
 
 def uploadScan(format: str, code: str):
     headers = {"Content-Type": "application/json"}
     data = {"content": code, "format": format}
     r = requests.post(f"{base_url}/api/v1/scan", json=data, headers=headers)
-    print(r.content, r.status_code)
+    return r
 
 
 def findFormat(file: str) -> str:
@@ -43,8 +43,15 @@ def findFormat(file: str) -> str:
     return format
 
 
+def printResults(json_r: dict) -> None:
+    if not json_r["summary"]["critical"]:
+        print("No critical vulns found!")
+
+
 if __name__ == "__main__":
     checkHealth(base_url=base_url)
     format = findFormat(file)
     code = loadFile(file)
-    uploadScan(format, code)
+    result = uploadScan(format, code)
+    json_r = result.json()
+    printResults(json_r)
